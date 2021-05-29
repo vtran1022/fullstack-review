b36d2fc57c516a7922904c2da7f715eee7c265fb
Vy Tran <vtran1022@Gmail.com>
2021-05-28 21:33:02 -0700

    Worked on db index.js file, created schema and implemented save func

--- database/index.js
+++ database/index.js
@@ -1,16 +1,37 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+//define a schema
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  // _id: new ObjectID(),
+  username: String, // the username we're searching for -- owner.login
+  repo_name: String, // repo's name -- name
+  repo_id: Number, // repo's id -- id
+  url: String, // repo's html url -- html_url
+  watchers: Number // repo's watcher count -- watchers_count
 });
 
+// compile our model - use this to create a document --> const newRepo = new Repo({....});
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  const newRepo = new Repo({
+    username: repo.username,
+    repo_name: repo.repo_name,
+    repo_id: repo.repo_id,
+    url: repo.url,
+    watchers: repo.watchers
+  })
+
+  newRepo.save((err) => {
+    if (err) {
+      console.log(`Error saving new repo: ${err}`);
+    }
+  });
 }
 
 module.exports.save = save;
