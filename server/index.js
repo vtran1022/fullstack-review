fe68d560a14733e56ca656bf0ca01146dcf56b79
Vy Tran <vtran1022@Gmail.com>
2021-05-28 22:36:48 -0700

    Implementing code for post req

--- server/index.js
+++ server/index.js
@@ -1,15 +1,42 @@
 const express = require('express');
 let app = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
+const { getReposByUsername } = require('../helpers/github.js');
+
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.urlencoded());
 
 app.post('/repos', function (req, res) {
-  console.log('post', req.body.username);
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  const user = req.body.username;
+
+  getReposByUsername(user, (results, err) => {
+    // console.log('kareeen', results);
+    // console.log('errrrrr', err);
+    if (err) {
+      res.status(400).send(err)
+    } else {
+      let cheese = results.map((repoObj) => {
+        const repo = {
+          username: repoObj.owner.login,
+          repo_name: repoObj.name,
+          repo_id: repoObj.id,
+          url: repoObj.html_url,
+          watchers: repoObj.watchers_count
+        };
+
+        return repo;
+      });
+      console.log(cheese);
+    }
+
+  });
+
 });
 
 app.get('/repos', function (req, res) {
